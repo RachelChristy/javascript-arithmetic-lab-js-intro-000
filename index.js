60c9e9d1c20ce6bebc63091869a7e4a3d3a55dc1
Rachel <rachel.green89@gmail.com>
2017-10-03 02:39:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ function dec(n){
 }
 
 function makeInt(n){
-  parseInt(n);
-  return n ;
+  return parseInt(n, 10);
+  
 }
 
 
 function preserveDecimal(n){
- parseFloat(n, 10);
- return n ;
+ return parseFloat(n, 10);
+  
 }
